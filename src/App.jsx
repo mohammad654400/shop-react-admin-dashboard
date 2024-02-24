d4c513abd5d124e6e548a5a22037a7d93b10b658
mohammad javad kenevisi <kenevisimohammadjavad@gmail.com>
2024-02-24 10:52:14 +0330

    add project and add login and register

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Login from "./features/identity/component/login";
+import Register from "./features/identity/component/register";
+function App() {
+  return (
+    <>
+      <Register></Register>
+    </>
+  );
+}
+
+export default App;
