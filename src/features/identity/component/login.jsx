431b17c5a01a6f5f6d4a8b61bbd93ea8037831d9
mohammad javad kenevisi <kenevisimohammadjavad@gmail.com>
2024-03-05 18:53:25 +0330

    login

--- src/features/identity/component/login.jsx
+++ src/features/identity/component/login.jsx
@@ -1,6 +1,23 @@
 import fasco from "@assets/images/fasco.png";
-import { Link } from "react-router-dom";
+import { useForm } from "react-hook-form";
+import { useTranslation } from "react-i18next";
+import { Link, redirect } from "react-router-dom";
+import { useSubmit } from "react-router-dom";
+import { httpService } from "../../../core/http-service";
 const Login = () => {
+  const { t } = useTranslation();
+  const {
+    register,
+    watch,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const submitForm = useSubmit();
+  const onSubmit = (data) => {
+    submitForm(data, { method: "post" });
+  };
+
   return (
     <>
       <div className="text-center mt-4">
@@ -10,14 +27,12 @@ const Login = () => {
             marginBottom: "30px",
           }}
         />
-        <h1 className="h2">پنل ادمین فروشگاه ری اکت</h1>
+        <h1 className="h2">{t(`login.title`)}</h1>
+        <p className="lead">{t(`login.description`)} </p>
         <p className="lead">
-          جهت ورود لازم است از طریق موبایل و رمز عبور خود اقدام کنید
-        </p>
-        <p className="lead">
-          قبلا ثبت نام نکرده اید؟
+          {t(`login.question`)}
           <Link to="/register" className="me-2">
-            ثبت نام کنید
+            {t(`login.register`)}
           </Link>
         </p>
       </div>
@@ -25,21 +40,54 @@ const Login = () => {
       <div className="card">
         <div className="card-body">
           <div className="m-sm-4">
-            <form>
+            <form onSubmit={handleSubmit(onSubmit)}>
               <div className="mb-3">
-                <label className="form-label">موبایل</label>
-                <input className="form-control form-control-lg" />
+                <label className="form-label">{t(`login.mobile`)}</label>
+                <input
+                  {...register("mobile", {
+                    required: `${t("login.errorMobile")}`,
+                    minLength: 11,
+                    maxLength: 11,
+                  })}
+                  className={`form-control form-control-lg ${
+                    errors.mobile && `is-invalid`
+                  }`}
+                />
+
+                {errors.mobile && errors.mobile.type === "required" && (
+                  <p className="text-danger small fw-bolder mt-1">
+                    {errors.mobile?.message}
+                  </p>
+                )}
+
+                {errors.mobile &&
+                  (errors.mobile.type === "minLength" ||
+                    errors.mobile.type === "maxLength") && (
+                    <p className="text-danger small fw-bolder mt-1">
+                      {t("login.errorPassword")}
+                    </p>
+                  )}
               </div>
               <div className="mb-3">
-                <label className="form-label">رمز عبور</label>
+                <label className="form-label">{t(`login.password`)}</label>
                 <input
-                  className="form-control form-control-lg mb-2"
+                  {...register("password", {
+                    required: `${t("login.password")}`,
+                  })}
+                  className={`form-control form-control-lg ${
+                    errors.password && `is-invalid`
+                  }`}
                   type="password"
                 />
+                {errors.password && errors.password.type === "required" && (
+                  <p className="text-danger small fw-bolder mt-1">
+                    {errors.password?.message}
+                  </p>
+                )}
               </div>
               <div className="text-center mt-3">
                 <button type="submit" className="btn btn-lg btn-primary">
-                  وارد شوید
+                  {t(`login.signin`)}
                 </button>
               </div>
             </form>
@@ -50,3 +98,17 @@ const Login = () => {
   );
 };
 export default Login;
+
+export async function loginAction({ request }) {
+  const formData = await request.formData();
+  const data = Object.fromEntries(formData);
+  const response = await httpService.post("/login", data);
+  console.log(response);
+  if (response.status === 200) {
+    if (response.data && response.data.token) {
+      localStorage.setItem("token", response.data.token);
+    }
+
+    return redirect("/");
+  }
+}
